9649a58eb0bd6f0b186fe70999adbdb0dacaf72f
Sean Phelan <seanphelan97@gmail.com>
2018-10-01 13:19:48 -0600

    all components working

--- src/components/guess-history.js
+++ src/components/guess-history.js
@@ -1,14 +1,26 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import Guess from './guess';
 
 export default function GuessHistory(props) {
+  const guessListElements = props.guesses.map((guess, index) => {
+    return <Guess index={index} />;
+  });
+
   return (
     <div>
       <ul>
-        
+        {guessListElements}
       </ul>
     </div>
   );
 }
 
+GuessHistory.propTypes = {
+  guesses: PropTypes.arrayOf(Guess)
+}
+
+GuessHistory.defaultProps = {
+  guesses: []
+}
--- src/components/guess.js
+++ src/components/guess.js
@@ -2,12 +2,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export function Guess(props) {
+export default function Guess(props) {
   return (
-    
+    <li key={props.index}>{props.number}</li>
   );
 }
 
 Guess.propTypes = {
-  number: PropTypes.number.isRequired
+  number: PropTypes.number.isRequired,
+  index: PropTypes.number.isRequired
 };
